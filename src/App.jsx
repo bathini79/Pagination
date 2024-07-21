d7390341c521a69c633bdce3a6cea2d629bfeeea
Bathini Nipun <116077457+bathini79@users.noreply.github.com>
2024-07-21 11:09:46 +0000

    Feat:products data pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,65 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [productData, setProductData] = useState([])
+  const [page,setPage] = useState(0)
+  const [perPage,setPerPage] = useState(10)
+
+  const [totalLength,setTotalLength] = useState(0)
+  const [loader,setLoader]=useState(true)
+  const getProducts = async () => {
+    try {
+      setLoader(false)
+      const response = await fetch(`https://dummyjson.com/products?limit=${(page+1)*perPage - (page*perPage)}&skip=${page*perPage}`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      const products  = await response.json();
+      setProductData(products.products)
+      setTotalLength(products?.total)
+      setLoader(true)
+
+    } catch (error) {
+      console.error("Fetch error: ", error);
+    }
+  };
+  useEffect(() => {
+    getProducts()
+  }, [page,perPage])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className='container'>
+      <div className="sub-container">
+      <div className="product-container">
+        <h3>Products</h3>
+        {/* {
+          productData ? productData?.slice(page*perPage,(page+1)*perPage)?.map((product,i) => {
+            return (<div>{product.id}.{product.title}</div>)
+          }) : ""
+        } */}
+        {
+          loader ? productData?.map((product,i) => {
+            return (<div>{product.id}.{product.title}</div>)
+          }) : ""
+        }
+      </div>
+      <div className='pagination-container'>
+        <button onClick={()=>setPage(page-1)}>&lt;</button>
+         {Array(Math.ceil(totalLength/perPage))?.fill()?.map((_,i)=>{
+          return(
+            <div className={`page ${i==(page) ? 'active':''}`} onClick={()=>setPage(i)}>{i+1}</div>
+          )
+         })}
+        <button onClick={()=>setPage(page+1)}>&gt;</button>
+        <select onClick={(e)=>setPerPage(e.target.value)}>
+        {[10,20,30].map((option, index) => (
+          <option key={index}  >
+            {option}
+          </option>
+        ))}
+        </select>
+      </div>
+      </div>
     </div>
   );
 }
